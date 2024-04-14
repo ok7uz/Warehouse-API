6033d7494ff1d30bd65c60e7be8a31984780efe5
ok7uz <komronbekobloev@gmail.com>
2024-04-14 14:01:51 +0500

    added pagination

--- apps/product/views.py
+++ apps/product/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework.pagination import PageNumberPagination
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
@@ -11,23 +12,35 @@ from apps.product.models import Inventory, Product
 from apps.product.serialzers import InventorySerializer, ProductSerializer
 
 
-class ProductListView(APIView):
+class ProductListView(APIView, PageNumberPagination):
     permission_classes = (AllowAny,)
     serializer_class = ProductSerializer
+    page_size = 25
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
-    @swagger_auto_schema(manual_parameters=[openapi.Parameter('currency', openapi.IN_QUERY, type=openapi.TYPE_STRING)],
-                         responses={200: serializer_class(many=True)},
-                         tags=['Product'])
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('page', openapi.IN_QUERY, type=openapi.TYPE_INTEGER),
+            openapi.Parameter('page_size', openapi.IN_QUERY, type=openapi.TYPE_INTEGER),
+            openapi.Parameter('currency', openapi.IN_QUERY, type=openapi.TYPE_STRING)
+        ],
+        responses={200: serializer_class},
+        tags=['Product']
+    )
     def get(self, request):
         products = Product.objects.all()
         filter = ProductFilter(request.GET, queryset=products)
         queryset = filter.qs if filter.is_valid() else products.none()
-        serializer = self.serializer_class(queryset, context={'request': request}, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        page = self.paginate_queryset(queryset, request)
+        serializer = self.serializer_class(page, context={'request': request}, many=True)
+        return self.get_paginated_response(serializer.data)
     
-    @swagger_auto_schema(request_body=serializer_class(),
-                         responses={200: serializer_class()},
-                         tags=['Product'])
+    @swagger_auto_schema(
+        request_body=serializer_class,
+        responses={200: serializer_class},
+        tags=['Product']
+    )
     def post(self, request, format=None):
         serializer = self.serializer_class(data=request.data)
 
@@ -41,15 +54,15 @@ class ProductView(APIView):
     permission_classes = (AllowAny,)
     serializer_class = ProductSerializer
 
-    @swagger_auto_schema(responses={200: serializer_class()},
+    @swagger_auto_schema(responses={200: serializer_class},
                          tags=['Product'])
     def get(self, request, product_id):
         product = get_object_or_404(Product, id=product_id)
         serializer = self.serializer_class(product, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    @swagger_auto_schema(request_body=serializer_class(),
-                         responses={200: serializer_class()},
+    @swagger_auto_schema(request_body=serializer_class,
+                         responses={200: serializer_class},
                          tags=['Product'])
     def put(self, request, product_id):
         product = get_object_or_404(Product, id=product_id)
