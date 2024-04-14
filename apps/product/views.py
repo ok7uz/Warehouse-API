0d93d98c3c1585068cbdbc94c9a55540d3f020ab
ok7uz <komronbekobloev@gmail.com>
2024-04-14 14:29:33 +0500

    changed pagination data key

--- apps/product/views.py
+++ apps/product/views.py
@@ -48,6 +48,15 @@ class ProductListView(APIView, PageNumberPagination):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_paginated_response(self, data):
+
+        return Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'count': self.page.paginator.count,
+            'data': data,
+        })
     
 
 class ProductView(APIView):
