1463312652507c6d779ff34a74991f2e71baabd1
Komronbek Obloev <89721936+ok7uz@users.noreply.github.com>
2024-04-24 16:06:15 +0500

    urls changed

--- apps/purchase/urls.py
+++ apps/purchase/urls.py
@@ -7,5 +7,5 @@ app_name = 'purchase'
 urlpatterns = [
     path('', PurchaseListView.as_view(), name='purchases'),
     path('consignment/', ConsignmentListView.as_view(), name='consignment'),
-    path('provider/', ProviderListView.as_view(), name='providers'),
+    path('providers/', ProviderListView.as_view(), name='providers'),
 ]
