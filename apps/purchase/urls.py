372eab22ff284a5a5b12ebf373628e09561e3164
Komronbek Obloev <89721936+ok7uz@users.noreply.github.com>
2024-04-26 15:52:35 +0500

    added purchase history & purchase filters

--- apps/purchase/urls.py
+++ apps/purchase/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 
-from apps.purchase.views import ProviderDetailView, ProviderListView, PurchaseListView, ConsignmentListView
+from apps.purchase.views import (
+    ProviderDetailView,
+    ProviderListView,
+    PurchaseListView,
+    ConsignmentListView,
+    PurchaseHistoryView
+)
 
 app_name = 'purchase'
 
 urlpatterns = [
     path('', PurchaseListView.as_view(), name='purchases'),
     path('consignment/', ConsignmentListView.as_view(), name='consignment'),
-    path('history/', ConsignmentListView.as_view(), name='history'),
+    path('history/', PurchaseHistoryView.as_view(), name='history'),
     path('providers/', ProviderListView.as_view(), name='providers'),
     path('providers/<int:provider_id>/', ProviderDetailView.as_view(), name='provider-detail')
 ]
