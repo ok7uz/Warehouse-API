1767a41769a0e094c3600215d4e03021b717e7dc
Komronbek Obloev <89721936+ok7uz@users.noreply.github.com>
2024-04-27 11:38:24 +0500

    product filter

--- apps/product/filters.py
+++ apps/product/filters.py
@@ -3,11 +3,10 @@ import django_filters.rest_framework as filters
 
 
 class ProductFilter(filters.FilterSet):
+    name = filters.CharFilter(lookup_expr='icontains')
+    barcode = filters.CharFilter(lookup_expr='icontains')
+    currency = filters.CharFilter(lookup_expr='exact')
 
     class Meta:
         model = Product
-        fields = {
-            'currency': ['exact'], 
-            'name': ['icontains'],
-            'barcode': ['icontains'],
-        }
+        fields = ['name', 'barcode', 'currency']
--- apps/warehouse/filters.py
+++ apps/warehouse/filters.py
@@ -6,9 +6,8 @@ from apps.warehouse.models import WarehouseProduct
 class WarehouseProductFilter(filters.FilterSet):
     name = filters.CharFilter(field_name='product__name', lookup_expr='icontains')
     barcode = filters.NumberFilter(field_name='product__barcode', lookup_expr='icontains')
+    currency = filters.CharFilter(field_name='product__currency', lookup_expr='exact')
 
     class Meta:
         model = WarehouseProduct
-        fields = {
-            'product__currency': ['exact'],
-        }
+        fields = ['name', 'barcode', 'currency']
