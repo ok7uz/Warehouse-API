23b8f02370d31daf306f1f9c3744575d23ad7e5b
ok7uz <komronbekobloev@gmail.com>
2024-05-05 13:12:42 +0500

    store_id

--- apps/product/serializers.py
+++ apps/product/serializers.py
@@ -1,9 +1,20 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
 from apps.product.models import Product
+from apps.store.models import Store
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    store_id = serializers.UUIDField(source='store.id')
+
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = [
+            'id', "store_id", "name", "currency", "purchasing_price", "markup_percentage", "selling_price",
+            "wholesale_price", "created", "modified",
+        ]
+
+    def create(self, validated_data):
+        store = get_object_or_404(Store, id=validated_data.pop('store')['id'])
+        return Product.objects.create(store=store, **validated_data)
